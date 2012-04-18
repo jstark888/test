f22a1919bbeb73ab165bcdd6966f4f6125dbfcfe
jstark <jstark@JStark-Laptop.iriswireless.com>
2012-04-18 07:22:28 -1000

    Added lval field

--- my_test1/src/jstark/test/git/Test1.java
+++ my_test1/src/jstark/test/git/Test1.java
@@ -2,10 +2,11 @@ package jstark.test.git;
 
 public class Test1 extends Test {
 	private int val;
+	private long lval;
 	
 	public Test1(int val) {
 		this.val = val;
-		setVersion("1.0");
+		setVersion("1.1");
 	}
 	
 	public int getInt() {
@@ -15,4 +16,13 @@ public class Test1 extends Test {
 	public void setInt(int val) {
 		this.val = val;
 	}
+
+	public long getLongVal() {
+		return lval;
+	}
+
+	public void setLongVal(long lval) {
+		this.lval = lval;
+	}
+	
 }
