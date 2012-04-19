b74dee65f7b716c4535da1ab04cd528301f1783c
jstark <jstark@JStark-Laptop.iriswireless.com>
2012-04-19 06:50:56 -1000

    Initial check in

---
+++ my_test2/src/jstark/test2/git/A.java
@@ -0,0 +1,20 @@
+package jstark.test2.git;
+
+public class A {
+
+	private String name;
+	
+	public A(String name) {
+		this.setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
