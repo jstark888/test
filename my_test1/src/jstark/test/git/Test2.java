9033b4d407a93e60e32856f57be1a6cb2b063ecb
jstark <jstark@JStark-Laptop.iriswireless.com>
2012-04-18 14:00:27 -1000

    Add field sval

--- my_test1/src/jstark/test/git/Test2.java
+++ my_test1/src/jstark/test/git/Test2.java
@@ -3,10 +3,11 @@ package jstark.test.git;
 public class Test2 extends Test {
 
 	private byte val;
+	private String sval;
 	
 	public Test2(byte val) {
 		this.val = val;
-		setVersion("1.0");
+		setVersion("1.2");
 	}
 	
 	public byte getByte() {
@@ -16,4 +17,14 @@ public class Test2 extends Test {
 	public void setByte(byte val) {
 		this.val = val;
 	}
+
+	public String getString() {
+		return sval;
+	}
+
+	public void setString(String sval) {
+		this.sval = sval;
+	}
+	
+	
 }
